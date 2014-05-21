f981486539ee75f7ca49bae52502a5f0965c43cb
Aayush Karwatkar <aayushkarwatkar@gmail.com>
2014-05-22 01:29:56 +0530

    Init

---
+++ IntentsLab/src/course/labs/intentslab/ExplicitlyLoadedActivity.java
@@ -0,0 +1,73 @@
+package course.labs.intentslab;
+
+import android.app.Activity;
+import android.app.Instrumentation.ActivityResult;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.ExtractedText;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ExplicitlyLoadedActivity extends Activity {
+
+	static private final String TAG = "Lab-Intents";
+	
+
+	private EditText mEditText;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.explicitly_loaded_activity);
+
+		// Get a reference to the EditText field
+		mEditText = (EditText) findViewById(R.id.editText);
+
+		// Declare and setup "Enter" button
+		Button enterButton = (Button) findViewById(R.id.enter_button);
+		enterButton.setOnClickListener(new OnClickListener() {
+
+			// Call enterClicked() when pressed
+
+			@Override
+			public void onClick(View v) {
+
+				enterClicked();
+			
+			}
+		});
+
+	}
+
+	// Sets result to send back to calling Activity and finishes
+	
+	private void enterClicked() {
+
+		Log.i(TAG,"Entered enterClicked()");
+		
+		String test = new String(mEditText.getText().toString());
+		
+		Intent intent3 = new Intent();
+		
+		intent3.putExtra("text", test);
+		
+		setResult(RESULT_OK, intent3);
+		
+		finish();
+	
+		
+		
+		// TODO - Save user provided input from the EditText field
+
+		// TODO - Create a new intent and save the input from the EditText field as an extra
+		
+		// TODO - Set Activity's result with result code RESULT_OK
+		
+		// TODO - Finish the Activity
+
+	}
+}
